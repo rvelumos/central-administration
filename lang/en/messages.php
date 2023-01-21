2c8293edbe21ddc9598c896eeead64a6076b697b
Ronald <ronald.eijsden@gmail.com>
2023-01-21 21:52:01 +0100

    prepare language messages.php NL/EN & welcome page changes

---
+++ lang/en/messages.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to our application!',
+    'cancel' => 'Cancel',
+    'add' => 'Add',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'view' => 'View',
+    'save' => 'Save',
+    'title' => 'Title',
+    'previous' => 'Previous',
+    'next' => 'Next',
+    'search' => 'Search',
+    'user' => 'User',
+    'admin' => 'Administrator',
+    'download_file' => 'Download file',
+    'edit_file' => 'Edit file',
+    'delete_file' => 'Delete file',
+    'sort' => 'Sort',
+    'filter' => 'Filter',
+    'settings' => 'Settings',
+    'accounts' => 'Accounts',
+    'login' => 'Login',
+    'logout' => 'Logout',
+];
+
+?>
---
+++ lang/nl/messages.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'welcome' => 'Welkom in het administratiepaneel!',
+    'cancel' => 'Annuleer',
+    'add' => 'Toevoegen',
+    'edit' => 'Bewerken',
+    'delete' => 'Verwijderen',
+    'view' => 'Bekijken',
+    'save' => 'Opslaan',
+    'title' => 'Titel',
+    'previous' => 'Vorige',
+    'next' => 'Volgende',
+    'search' => 'Zoeken',
+    'user' => 'Gebruiker',
+    'admin' => 'Administrator',
+    'download_file' => 'Download bestand',
+    'edit_file' => 'Bestand bewerken',
+    'delete_file' => 'Bestand verwijderen',
+    'sort' => 'Sorteren',
+    'filter' => 'Filteren',
+    'settings' => 'Instellingen',
+    'accounts' => 'Accounts',
+    'login' => 'Inloggen',
+    'logout' => 'Uitloggen',
+];
+
+?>
